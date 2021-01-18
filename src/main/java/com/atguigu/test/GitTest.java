abe5d76789519492aab1effedaa2f5c4c95cbf60
XiaoXin52 <50975483+XiaoXin52@users.noreply.github.com>
2021-01-18 20:28:24 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("333333333333");
         System.out.println("444444444444444");
         System.out.println("5555555555");
+        System.out.println("6666666666");
     }
 }
